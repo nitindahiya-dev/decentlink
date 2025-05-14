030ceb6418186341b7408cb1f31ef75af52ac097
nitindahiya-dev <nitindahiya00000@gmail.com>
2025-05-14 17:30:42 +0530

    refactor: Update prisma client initialization to support global instance and logging

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
-// src/lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-export { prisma };
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+export const prisma =
+  global.prisma ??
+  new PrismaClient({ log: ['query'] });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
